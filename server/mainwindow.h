586a1fd9424f7d577062d01d837997cbbe52e551
1-MR-RaBBit-1 <163560416+1-MR-RaBBit-1@users.noreply.github.com>
2026-09-04 19:01:20 +0330

    Update mainwindow.h

--- server/mainwindow.h
+++ server/mainwindow.h
@@ -1,134 +1,43 @@
-#include "mainwindow.h"
-#include <QDataStream>
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+#include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QMessageBox>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QLabel>
+
+#include <Qt3DCore/QEntity>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DRender/QCamera>
+#include <QColor>
+#include <QTimer>
+#include <Qt3DRender/QMesh>
+#include <Qt3DRender/QTexture>
+#include <Qt3DExtras/QTextureMaterial>
+#include <QFile>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+class MainWindow : public QMainWindow
 {
-    auto *view = new Qt3DExtras::Qt3DWindow();
-    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity();
-    view->setRootEntity(rootEntity);
-    QWidget *Container = QWidget::createWindowContainer(view);
-    
-    QWidget *centralW = new QWidget(this);
-    setCentralWidget(centralW);
-    QVBoxLayout *VB1 = new QVBoxLayout(centralW);
-    QLineEdit *message = new QLineEdit(this);
-    message->setPlaceholderText("no message yet ...");
-    message->setReadOnly(true);
+    Q_OBJECT
 
-    QGridLayout *control = new QGridLayout();
-    
-    QLabel *label1 = new QLabel("ip address:");
-    control->addWidget(label1,0,0);
-    QLineEdit *IpC = new QLineEdit();
-    control->addWidget(IpC,0,1);
-    QPushButton *Connect = new QPushButton();
-    Connect->setText("Connect");
-    control->addWidget(Connect, 0,2);
-    QPushButton *addM = new QPushButton();
-    addM->setText("add Metorate");
-    control->addWidget(addM, 1,0);
-    QLabel *label2 = new QLabel("metorate Name:");
-    control->addWidget(label2,1,1);
-    QLineEdit *metorateName = new QLineEdit();
-    control->addWidget(metorateName, 1,2);
-    QPushButton *Fire = new QPushButton();
-    Fire->setText("Fire");
-    control->addWidget(Fire,1,3);
-    
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
-    VB1->addWidget(Container);
-    VB1->addWidget(message);
-    VB1->addLayout(control);
+private:
+    QTcpServer *server;
+};
 
-    // Camera
-    auto *camera = view->camera();
-
-    camera->setPosition(
-        QVector3D(0, 0, 20)
-    );
-
-    camera->setViewCenter(
-        QVector3D(0, 0, 0)
-    );
-    
-
-    Qt3DRender::QMesh *earthM = new Qt3DRender::QMesh();
-    earthM->setSource(QUrl("qrc:/models/earth.obj"));
-    Qt3DCore::QEntity *earthE = new Qt3DCore::QEntity(rootEntity);
-
-    Qt3DCore::QTransform *ETransform = new Qt3DCore::QTransform();
-    ETransform->setScale(0.5f);
-    ETransform->setTranslation(QVector3D(-3, 0, 0));
-
-    Qt3DRender::QTexture2D *ETexture = new Qt3DRender::QTexture2D();
-    Qt3DRender::QTextureImage *ETextureImage = new Qt3DRender::QTextureImage();
-    ETextureImage->setSource(QUrl("qrc:/models/earth.png"));
-    ETexture->addTextureImage(ETextureImage);
-    Qt3DExtras::QTextureMaterial *EMaterial= new Qt3DExtras::QTextureMaterial();
-    EMaterial->setTexture(ETexture);
-
-    earthE->addComponent(earthM);
-    earthE->addComponent(ETransform);
-    earthE->addComponent(EMaterial);
-
-    connect(addM,&QPushButton::clicked,this,[=](){
-        Qt3DRender::QMesh *metoriteM = new Qt3DRender::QMesh();
-        metoriteM->setSource(QUrl("qrc:/models/metorate.obj"));
-        Qt3DCore::QEntity *metoriteE = new Qt3DCore::QEntity(rootEntity);
-        // metoriteM->setRadius(0.5f);
-        Qt3DCore::QTransform *transform = new Qt3DCore::QTransform();
-        transform->setScale(0.9f);
-        Qt3DRender::QTexture2D *texture = new Qt3DRender::QTexture2D();
-
-        Qt3DRender::QTextureImage *textureImage =
-            new Qt3DRender::QTextureImage();
-
-        textureImage->setSource(
-            QUrl("qrc:/models/meteor_texture.png")
-        );
-
-        texture->addTextureImage(textureImage);
-
-        Qt3DExtras::QTextureMaterial *material =
-            new Qt3DExtras::QTextureMaterial();
-
-        material->setTexture(texture);
-        // material1->setDiffuse(QColor(255,0,0));
-
-        metoriteE->addComponent(metoriteM);
-        metoriteE->addComponent(transform);
-        metoriteE->addComponent(material);
-    });
-
-
-    setWindowTitle("planet War");
-    resize(500,500);
-
-
-
-    server = new QTcpServer(this);
-
-    server->listen(QHostAddress::LocalHost, 5000);
-
-    qDebug() << "Server is listening...";
-    
-    connect(server, &QTcpServer::newConnection, this, [=]() {
-
-        QTcpSocket *clientSocket = server->nextPendingConnection();
-
-        connect(clientSocket, &QTcpSocket::readyRead, this, [=](){
-            QDataStream stream(clientSocket);
-            QString text;
-            stream >> text;
-            message->setText(text);
-            // QMessageBox::information(this,"Received Data",text);
-            // qDebug() << text;
-        });
-
-        qDebug() << "Client connected!";
-
-    });
-}
-
-MainWindow::~MainWindow() = default;
+#endif // MAINWINDOW_H
